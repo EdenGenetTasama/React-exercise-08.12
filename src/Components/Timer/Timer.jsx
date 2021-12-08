ea431865d5af78c74040e9c27bd32ea874b1f1f3
EdenGenetTasama <edentasama19@gmail.com>
2021-12-08 14:47:11 +0200

    first commit

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,47 +1,51 @@
 import React from "react";
 
-
-class Timer extends React.Component{
-constructor(props){
-super(props);
-    this.state = {second : 0}
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { second: 0, text: "" };
     this.secondUp = this.secondUp.bind(this);
     this.secondDown = this.secondDown.bind(this);
     this.reStart = this.reStart.bind(this);
-}
+    // this.showInupt = this.showInupt.bind(this);
+  }
 
-secondUp(){
-    const nextSecond = this.state.second + 1 ; 
-    this.setState({second : nextSecond});
-}
+  secondUp() {
+    const nextSecond = this.state.second + 1;
+    this.setState({ second: nextSecond });
+  }
 
-secondDown(){
-    const downSecond = this.state.second -1; 
-    this.setState({second : downSecond});
+  secondDown() {
+    const downSecond = this.state.second - 1;
+    this.setState({ second: downSecond });
+  }
 
-}
+  reStart() {
+    this.setState({ second: 0 });
+  }
 
-reStart(){
+  //   showInupt() {
+  //     this.setState({ text: this.state.text  });
+  //   }
 
-    this.setState({second : 0})
-}
-
-
-render(){
-    return < div>
-    <h1>{this.state.second}</h1>
-    <input type="text" value="" onChange={} />
-    <button onClick={this.secondUp}>Up</button>
-    <button onClick={this.secondDown}>Down</button>
-    <button onClick={this.reStart}>restart</button>
-
-
-    
-    </div>
-}
-
-
-
+  render() {
+    return (
+      <div>
+        <h1>{this.state.second}</h1>
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={(event) => {
+            this.setState({ text: event.target.value });
+            console.log(event.target.value);
+          }}
+        />
+        <button onClick={this.secondUp}>Up</button>
+        <button onClick={this.secondDown}>Down</button>
+        <button onClick={this.reStart}>restart</button>
+      </div>
+    );
+  }
 }
 
 export default Timer;
